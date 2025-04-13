0bfb1179ebf435cf96c32a82e9e768d20c08608e
Maxiflexy <onyemax247@gmail.com>
2025-04-13 13:11:17 +0100

    added ai integration from germini

--- ai-service/src/main/java/com/maxiflexy/aiservice/service/ActivityMessageListener.java
+++ ai-service/src/main/java/com/maxiflexy/aiservice/service/ActivityMessageListener.java
@@ -1,6 +1,7 @@
 package com.maxiflexy.aiservice.service;
 
 import com.maxiflexy.aiservice.model.Activity;
+import com.maxiflexy.aiservice.repository.RecommendationRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -13,10 +14,14 @@ import org.springframework.stereotype.Service;
 public class ActivityMessageListener {
 
     private final ActivityAIService aiService;
+    private final RecommendationRepository recommendationRepository;
 
     @RabbitListener(queues = "activity.queue")
     public void processActivity(Activity activity){
         log.info("Received activity for processing: {}", activity.getId());
-        log.info("Generated Recommendation: {}", aiService.generateRecommendation(activity));
+        //log.info("Generated Recommendation: {}", aiService.generateRecommendation(activity));
+
+        var recommendation = aiService.generateRecommendation(activity);
+        recommendationRepository.save(recommendation);
     }
 }
--- ai-service/src/main/java/com/maxiflexy/aiservice/service/RecommendationService.java
+++ ai-service/src/main/java/com/maxiflexy/aiservice/service/RecommendationService.java
@@ -3,7 +3,6 @@ package com.maxiflexy.aiservice.service;
 import com.maxiflexy.aiservice.model.Recommendation;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface RecommendationService {
     List<Recommendation> getUserRecommendation(String userId);
